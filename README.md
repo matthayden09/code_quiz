71cfead65b9e0d03ecb7155c638324a8ee9ad0cc
Matthew Hayden <matthayden09@gmail.com>
2020-12-03 22:28:30 -0600

    defined variables and funtions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # code_quiz
+
+Note to Grader:
+
+I'm honestly embarrassed to submit such unfinished work but this assignment got the better of me. I will be re-submitting as soon as I have a better grasp of JavaScript and JQuery. Any comments on where to start or how to structure my layout differently will be very much appreciated.
--- script.js
+++ script.js
@@ -1,51 +1,74 @@
-// GIVEN I am taking a code quiz
-(function (){
-
-    // variables
-    var questions = [
-        {
-            "title": "Question 1",
-            "choices": {
-                "a": "Answer 1",
-                "b": "Answer 2",
-                "c": "Answer 3",
-            },
-            "answer": "a"
+// variables
+var questions = [
+    {
+        "title": "Question 1",
+        "choices": {
+            "a": "Answer 1",
+            "b": "Answer 2",
+            "c": "Answer 3",
         },
-        {
-            "title": "Question 2",
-            "choices": {
-                "a": "Answer 1",
-                "b": "Answer 2",
-                "c": "Answer 3",
-            },
-            "answer": "b"
+        "answer": "a"
+    },
+    {
+        "title": "Question 2",
+        "choices": {
+            "a": "Answer 1",
+            "b": "Answer 2",
+            "c": "Answer 3",
         },
-        {
-            "title": "Question 3",
-            "choices": {
-                "a": "Answer 1",
-                "b": "Answer 2",
-                "c": "Answer 3",
-            },
-            "answer": "c"
-        }
-    ];
-    var answers = [];
-    var startButton = document.getElementById('start');
-    var nextButton = document.getElementById('next');
-    var submitButton = document.getElementById('submit');
+        "answer": "b"
+    },
+    {
+        "title": "Question 3",
+        "choices": {
+            "a": "Answer 1",
+            "b": "Answer 2",
+            "c": "Answer 3",
+        },
+        "answer": "c"
+    }
+];
+var answers
+var startButton = $('#start');
+var nextButton = $('#next');
+var submitButton = $('#submit');
 
-    // event listeners
-    startButton.addEventListener('click');
-    nextButton.addEventListener('click');
-    submitButton.addEventListener('click');
+// show quiz after start is selected
+function showQuiz(event) {
 
-    // timer
-    
+}
 
-   
+// move to next question
+function nextQuestion(event) {
 
-    
+}
 
-})();
+// submit answers
+function submitAnswers(event) {
+
+}
+
+// show total score
+
+// enter name
+
+// make a countdown timer
+
+// event listeners
+startButton.on('click', function (showQuiz) {
+});
+nextButton.on('click', function (nextQuestion) {
+});
+submitButton.on('click', function (submitAnswers) {
+});
+
+
+
+
+
+
+
+
+
+
+
